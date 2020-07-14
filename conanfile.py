78bb908f2368079d5fd35f85813d56c49ce7f31a
Steve Mokris <steve@kosada.com>
2020-07-13 22:02:50 -0400

    Update to Csu version 85, needed for ld64 version 253.3.  Use pip to install Conan, since it's faster and more reliable than Homebrew.  [#17561]

--- conanfile.py
+++ conanfile.py
@@ -4,24 +4,29 @@ import shutil
 class CsuConan(ConanFile):
     name = 'csu'
 
-    csu_version = '79'
-    package_version = '1'
+    csu_version = '85'
+    package_version = '0'
     version = '%s-%s' % (csu_version, package_version)
 
-    build_requires = 'llvm/3.3-5@vuo/stable'
+    build_requires = 'llvm/3.3-7@vuo/stable'
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'https://opensource.apple.com/'
     license = 'https://opensource.apple.com/source/Csu/Csu-%s/crt.c.auto.html' % csu_version
-    description = 'Combines several object files and libraries, resolves references, and produces an ouput file'
+    description = 'C runtime helpers'
     source_dir = 'Csu-%s' % csu_version
 
     def source(self):
         tools.get('http://www.opensource.apple.com/tarballs/Csu/Csu-%s.tar.gz' % self.csu_version,
-                  sha256='d052e1daa1f5de7fc02e7e7cb8b79ee2eeaad0f321c0a70bea4fc7217e232ec2')
+                  sha256='f2291d7548da854322acf194a875609bfae96c2481738cf6fd1d89eea9ae057a')
 
-        # crt.c contains the license in the first block comment.
-        # Truncate it at the end of that comment.
-        self.run('sed "/\\*\\//q" %s/crt.c > %s/%s.txt' % (self.source_dir, self.source_dir, self.name))
+        with tools.chdir(self.source_dir):
+            tools.replace_in_file('start.s',
+                                  '#include <Availability.h>',
+                                  '')
+
+            # crt.c contains the license in the first block comment.
+            # Truncate it at the end of that comment.
+            self.run('sed "/\\*\\//q" crt.c > %s.txt' % self.name)
 
     def build(self):
         with tools.chdir(self.source_dir):
@@ -30,8 +35,8 @@ class CsuConan(ConanFile):
                 'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
             }
             with tools.environment_append(env_vars):
-                self.run('make')
-            shutil.move('crt1.v3.o', 'crt1.o')
+                self.run('make crt1.v2.o')
+            shutil.move('crt1.v2.o', 'crt1.o')
 
     def package(self):
         self.copy('crt1.o', src=self.source_dir, dst='lib')
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,15 +1,14 @@
 from conans import ConanFile
 
 class CsuTestConan(ConanFile):
-    requires = 'llvm/3.3-5@vuo/stable'
-    generators = 'qbs'
+    requires = 'llvm/3.3-7@vuo/stable'
 
     def imports(self):
         self.copy('*', dst='bin', src='bin')
         self.copy('*', dst='lib', src='lib')
 
     def build(self):
-        self.run('bin/clang -nostartfiles ../../test_package.c -o test_package lib/crt1.o')
+        self.run('bin/clang -nostartfiles ../../test_package.c -o test_package lib/crt1.o -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk')
 
     def test(self):
         self.run('./test_package')
